30e12bdf04769f467c6e05c23b2823382c49880c
datngo <dat.ngotri.ncc@gmail.com>
2022-03-29 21:56:35 +0700

    init project

---
+++ src/test/java/com/datngo/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datngo.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
